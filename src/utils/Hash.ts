ab8f0b466ad43859963367b60f19e7e540a2ee70
Cedy Cadayong <cadayongcs@gmail.com>
2024-11-19 12:06:18 +0800

    feat: lint

--- src/utils/Hash.ts
+++ src/utils/Hash.ts
@@ -1,14 +1,17 @@
-import bcrypt from 'bcryptjs'
-import 'dotenv/config'
+import bcrypt from 'bcryptjs';
+import 'dotenv/config';
 
 const hashRounds = (): number => {
-    return Number(process.env.HASH_ROUNDS) || 8
-}
+  return Number(process.env.HASH_ROUNDS) || 8;
+};
 
 export const hash = async (str: string): Promise<string> => {
-    return await bcrypt.hash(str, hashRounds())
-}
+  return await bcrypt.hash(str, hashRounds());
+};
 
-export const verify = async (str: string, hashedStr: string): Promise<boolean> => { 
-    return await bcrypt.compare(str, hashedStr)
-}
+export const verify = async (
+  str: string,
+  hashedStr: string
+): Promise<boolean> => {
+  return await bcrypt.compare(str, hashedStr);
+};
